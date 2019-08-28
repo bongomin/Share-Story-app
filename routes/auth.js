aa6e0dc97ac0b96e2e3abe9fa8ea20e0d0aef0a5
bongomin <bongomindaniel@gmail.com>
2019-08-28 16:15:39 +0300

    feat(adding callback function for the google api):

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,11 @@ const passport = require('passport')
 router.get('/google',passport.authenticate('google',
 {scope:['profile','email']}));
 
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+    res.redirect('/dashboard');
+  });
+
 
 
 
