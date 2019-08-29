0f0f7f6ed6f43f720d722e93234a7712a41bbb23
bongomin <bongomindaniel@gmail.com>
2019-08-29 17:10:26 +0300

    feat(saving user to mongo):fetching users profile,firsName and LastName and Image

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load User Modle
+const User = mongoose.model('users');
 
 module.exports = function(passport){
    passport.use(
@@ -13,6 +15,33 @@ module.exports = function(passport){
      }, (accessToken, refreshToken, profile, done) => {
        console.log(accessToken);
        console.log(profile);
+      // const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+      
+      const newUser = {
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: profile.photos[0].value
+      }
+      // checking for exsiting user
+      User.findOne({
+         googleID  : profile.id
+
+      })
+      .then(user => {
+      if(user){
+         // returning user
+         done(null, user);
+      }else{
+         // create user
+         new User(newUser)
+         .save()
+         .then(user => done(null,user));
+
+      }
+      } )
+
      })
    )
  }
