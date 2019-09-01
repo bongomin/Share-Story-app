e3b186c7635b4531feb7af15fafc71bd2b2ee77e
bongomin <bongomindaniel@gmail.com>
2019-09-01 09:35:28 +0300

    feat(Restricting Accessin Routes): created Helpers folder that containes the auth file.
    - created the auth file with ensureAuthenticated odject that ensures the user is logged in or not.
    
    - created the ensureGuest Ogject that helps make sure is a user is already logged in, and he tries to go back to the index /welcome page, it will redirect the user to the dashboard instead . this only works if the use isAuthenticated into the app

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
+const {ensureAuthenticated ,ensureGuest} = require('../helpers/auth');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/',ensureGuest, (req, res) => {
   res.render('index');
 });
 
 /* GET DashBoard page. */
-router.get('/dashboard', function(req, res) {
+router.get('/dashboard',ensureAuthenticated, (req, res) => {
   res.render('index/dashboard');
 });
 
