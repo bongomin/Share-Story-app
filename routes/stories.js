7cd3d31b8d192ee197e2541266a9a6e95a190a1c
bongomin <bongomindaniel@gmail.com>
2019-09-02 14:04:12 +0300

    feat(fetching and dispalying public stories): designed the public stories GUI
    - fetched data from the stories router to be populated into the Public stories

--- routes/stories.js
+++ routes/stories.js
@@ -1,51 +1,50 @@
 const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
 const Story = mongoose.model('stories');
 const User = mongoose.model('users');
-const {ensureAuthenticated ,ensureGuest} = require('../helpers/auth');
-const router = express.Router();
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
-//   stories index
-router.get('/', function(req, res) {
-   res.render('stories/index');
- });
-
-//  add stories form
-
-router.get('/add',ensureAuthenticated, (req, res) => {
-   res.render('stories/add');
- });
-
-
-//  post request for stories
-// process adding stories to mongodb
-router.post('/',(req,res)=> {
- let allowComments;
-
- if(req.body.allowComments){
-   allowComments = true;
- }else{
-   allowComments = false;
- }
-
- const newStory = {
-   title : req.body.title,
-   body : req.body.body,
-   status : req.body.status,
-   allowComments :allowComments,
-   user : req.body.id
- }
-
-//  Creating new story
-new Story(newStory)
-.save()
-.then(story => { 
-  res.redirect(`/stories/show/${story.id}`);
+// Stories Index
+router.get('/', (req, res) => {
+  Story.find({status:"public"})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
 });
 
-
-
+// Add Story Form
+router.get('/add', ensureAuthenticated, (req, res) => {
+  res.render('stories/add');
 });
 
+// Process Add Story
+router.post('/', (req, res) => {
+  let allowComments;
+
+  if(req.body.allowComments){
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments:allowComments,
+    user: req.user.id
+  }
+
+  // Create Story
+  new Story(newStory)
+    .save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+});
 
 module.exports = router;
